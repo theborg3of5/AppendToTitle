ddfd6663e2e5614239fdad11b1858508fb95aabd
Gavin Borg <theborg3of5@gmail.com>
2017-05-07 22:25:55 -0500

    Reduce domains (take off the www.) to make domain matching more intuitive.

--- background.js
+++ background.js
@@ -37,16 +37,25 @@ function getSuffix(currentURL) {
 		return "";
 	
 	var urlObject = new URL(currentURL);
-	var domain = urlObject.hostname;
+	var domain = urlObject.hostname;	
 	if(!domain)
 		return "";
 	
-	for(var i = 0; i < LocalSettings.length; i++) {
-		if(LocalSettings[i].domain == domain)
+	var domainFromSettings, trimmedDomain;
+	for(var i = 0; i < LocalSettings.length; i++)
+		if(doDomainsMatch(LocalSettings[i].domain, domain))
 			return LocalSettings[i].suffix;
-	}
 	
 	return "";
+}
+function doDomainsMatch(domain1, domain2) {
+	// Reduce both (taking off www. if it's there) to check equality.
+	if(domain1.substring(0, 4) == "www.")
+		domain1 = domain1.substring(4);
+	if(domain2.substring(0, 4) == "www.")
+		domain2 = domain2.substring(4);
+	
+	return (domain1 == domain2);
 }
 
 
